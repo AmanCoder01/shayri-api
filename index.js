e367c2f4cd19b3ab7ecd2f00cc8d8edc3283b804
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-01-25 11:48:49 +0530

    last commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 
 const data = require("./data.json");
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hi this is demo made mr.aman");
 })
-app.get("/service",(req,res)=>{
+app.get("/service", (req, res) => {
     res.send(data);
 })
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log('Server is running on port 8000');
 })
